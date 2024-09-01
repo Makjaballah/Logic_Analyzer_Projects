2991652e6d78e1dea205698f2e28783eb8040eb0
Makjaballah <jaballahakram@gmail.com>
2024-09-01 18:37:56 +0100

    Creation of Logic_Analyzer_UART_Half-Duplex project folder

---
+++ 4_Logic_Analyzer_UART_Half-Duplex/Arduino_A/Arduino_A.ino
@@ -0,0 +1,26 @@
+/*
+ * Project_Name    :   UART_Half-Duplex
+ * File_Name       :   Arduino_A.ino
+ * Author          :   Mohamed Akram JABALLAH
+ * Target          :   Arduino Uno
+ * Environnement   :   Arduino IDE 2.1
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin (9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if  (Serial.available ()){
+  
+  Serial.println (Serial.readString ()); 
+ } 
+  delay (1000); 
+  Serial.println ("Hello Arduino B !");
+  
+}
